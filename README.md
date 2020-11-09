f387fadef4af757243595338cd3b4683315eafa7
aieke <287010613@qq.com>
2020-11-09 23:06:52 +0800

    a

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # dsaa
 This is a learning repository for data structures and algorithms
 
+[TOC]
 
 
-
+### sorts
 - [ ] [sorts/merge_sort](./sorts/merge_sort.py)
 - [ ] [sorts/bead_sort](./sorts/bead_sort.py)
 
--- sorts/merge_sort.py
+++ sorts/merge_sort.py
@@ -7,4 +7,35 @@
 # @Software: PyCharm
 
 
+def merge_sort(data):
+    """
 
+    :param data:
+    :return:
+
+    >>> merge_sort([3,4,5,2,1])
+    [1, 2, 3, 4, 5]
+
+    """
+    if len(data) < 2:
+        return data
+
+    def _merge(left, right):
+        def meg():
+            while left and right:
+                yield (left if left[0] < right[0] else right).pop(0)
+
+            yield from left
+            yield from right
+
+        return list(meg())
+
+    mid = len(data) // 2
+    return _merge(merge_sort(data[0:mid]), merge_sort(data[mid:]))
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    a = [3, 2, 4, 5, 1]
+    print(merge_sort(a))
